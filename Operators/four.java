975c987c6047701c2f8fbdcf246aa144705ca66f
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-05-19 10:16:05 +0530

    JAVA

---
+++ Operators/four.java
@@ -0,0 +1,10 @@
+class four
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 5;
+        int r = a%b;
+        System.out.println(r);
+    }
+}
---
+++ Operators/three.java
@@ -0,0 +1,10 @@
+class three
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 20;
+        int r = a*b;
+        System.out.println(r);
+    }
+}
