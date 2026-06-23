6ea56b8ecef1f24cfb228b101b88a0e912c39b52
vinaykumar2004vinay <vinaykumardasari2004@gmail.com>
2026-06-23 16:13:34 +0530

    JAVA

---
+++ Logical Operator/one.java
@@ -0,0 +1,14 @@
+class one
+{
+    public static void main(String args[])
+    {
+        float x = 9.8f;
+        float y = 7.3f;
+
+        int a = 5;
+        int b = 9;
+
+        boolean r = (x>y) && (a<b);
+        System.out.println(r);
+    }
+}
